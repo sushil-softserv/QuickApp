d9d2e291d5776a076244fabf835f67b007b41416
sushil-softserv <sushil@softserv.in>
2023-05-13 10:37:09 +0530

    Implement product repository

--- DAL/Repositories/Interfaces/IProductRepository.cs
+++ DAL/Repositories/Interfaces/IProductRepository.cs
@@ -9,11 +9,12 @@ using DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DAL.Repositories.Interfaces
 {
     public interface IProductRepository : IRepository<Product>
     {
-
+        Task<IEnumerable<Product>> GetAllProductsData(int page, int pageSize);
     }
 }
--- DAL/Repositories/ProductRepository.cs
+++ DAL/Repositories/ProductRepository.cs
@@ -21,7 +21,22 @@ namespace DAL.Repositories
         public ProductRepository(DbContext context) : base(context)
         { }
 
+        public async Task<IEnumerable<Product>> GetAllProductsData(int page, int pageSize)
+        {
+            IQueryable<Product> usersQuery = _appContext.Products;
 
+            if (page > 0)
+                usersQuery = usersQuery.Skip(page * pageSize);
+
+            if (pageSize != -1)
+                usersQuery = usersQuery.Take(pageSize);
+
+            //var products = await _appContext.Products.OrderBy(p => p.Id).Skip((page -1) * pageSize).Take(pageSize).ToListAsync();
+
+            var products = await usersQuery.OrderBy(p => p.Id).ToListAsync();
+
+            return products;
+        }
 
 
         private ApplicationDbContext _appContext => (ApplicationDbContext)_context;
